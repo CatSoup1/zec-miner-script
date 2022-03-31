25d4c7566acaf36d50e6e45595c5b1ef8f7f7223
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-03-31 18:10:44 +1000

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,5 +1,5 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz
 ls
-tar -xf xmrig-6.16.4-linux-x64.tar.gz
+tar -xvzf xmrig-6.16.4-linux-x64.tar.gz
 cd xmrig-6.16.4-linux-x64
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u ZEC:t1d3c5rnpATRbtypFa4exBQTsVtihheddoh.pc#rzex-599x -p x
